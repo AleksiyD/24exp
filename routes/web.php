9f19f120a0aecb120bf29c3bfd829a8ed8e8f83d
AleksiyD <50235593+AleksiyD@users.noreply.github.com>
2020-01-26 18:12:51 +0300

    add route

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,53 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('pages.index');
+});
+
+Route::get('article', function () {
+    return view('pages.article');
+});
+
+Route::get('articles', function () {
+    return view('pages.articles');
+});
+
+Route::get('contacts', function () {
+    return view('pages.contacts');
+});
+
+Route::get('document', function () {
+    return view('pages.document');
+});
+
+Route::get('documents', function () {
+    return view('pages.documents');
+});
+
+Route::get('landing', function () {
+    return view('pages.landing');
+});
+
+Route::get('news-standalone', function () {
+    return view('pages.news-standalone');
+});
+
+Route::get('news', function () {
+    return view('pages.news');
+});
+
+Route::get('review', function () {
+    return view('pages.review');
+});
+
+Route::get('services', function () {
+    return view('pages.services');
+})->name('services');
+
+Route::get('ty', function () {
+    return view('pages.ty');
+});
+
+Route::get('NotFound', function () {
+    return view('pages.404');
 });
